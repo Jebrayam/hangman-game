0e7392f16cfdfa8db813de4de05b9b87086831fd
Brayam Andres Saavedra Arce <brayamsaavedra@gmail.com>
2022-02-09 21:11:27 -0500

    Implementación de lógica de selección aleatoria de palabra y adición de nuevas palabra al juego

--- js/hangman.js
+++ js/hangman.js
@@ -3,4 +3,30 @@ var words = ["autumn", "bathroom", "body", "christmas", "clothes", "colours", "a
             "insects", "kitchen", "pencase", "pets", "school", "sport", "spring", "summer",
             "time", "town", "toys", "transport", "vegetables", "verbs", "weather", "winter"];
 
-function returnRandomWord(words){}
+var startBtn = document.querySelector("#startBtn");
+
+startBtn.addEventListener("click", function(){
+     var randomWord = returnRandomWord(words);  
+     console.log(words);     
+});
+
+var addBtn = document.querySelector("#addBtn");
+var inputField = document.querySelector("#inputField");
+
+addBtn.addEventListener("click", function(){
+    var newWord  = inputField.value;
+
+    if (newWord != ""){
+        words.push(newWord);
+        alert("New word added to the game.")
+    } else {
+        alert("Please enter a word.")
+    }
+    
+});
+
+function returnRandomWord(words){
+    var word = words[Math.floor(Math.random()*words.length)];
+
+    return word.toUpperCase();
+}
