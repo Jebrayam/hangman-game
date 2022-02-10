8cb023f61a1f87ab669e2eee289dfa3a4f308c35
Brayam Andres Saavedra Arce <brayamsaavedra@gmail.com>
2022-02-10 15:46:13 -0500

    Implementación del dibujado de las lineas y la posición de las letras acertadas

--- js/canvas.js
+++ js/canvas.js
@@ -10,3 +10,26 @@ background.src = "img/canvasBackground.jpg";
 background.onload = function(){
     brush.drawImage(background,0,0);   
 }
+
+function drawLines(wordLength){
+    let xInicial = 140;
+    let yInicial = 540;
+
+
+    for(let i=0; i < wordLength; i++){
+        brush.moveTo(xInicial, yInicial); 
+        brush.lineTo(xInicial + 20, yInicial);
+        brush.stroke();
+        xInicial += 30;
+    }   
+}
+
+function showRightLetter(letter, idxs){
+    let yInicial = 535;
+
+    idxs.forEach((idx) => {
+        let xInicial = 143;
+        xInicial += (30 * idx)
+        brush.fillText(letter, xInicial, yInicial);
+    });
+}
--- js/hangman.js
+++ js/hangman.js
@@ -1,3 +1,6 @@
+var splittedWord;
+var letterIdxs = [];
+
 var words = ["autumn", "bathroom", "body", "christmas", "clothes", "colours", "animals", 
             "drinks", "easter", "family", "feelings", "food", "fruits", "furniture", "halloween",
             "insects", "kitchen", "pencase", "pets", "school", "sport", "spring", "summer",
@@ -6,8 +9,13 @@ var words = ["autumn", "bathroom", "body", "christmas", "clothes", "colours", "a
 var startBtn = document.querySelector("#startBtn");
 
 startBtn.addEventListener("click", function(){
-     var randomWord = returnRandomWord(words);  
-     console.log(words);     
+    var randomWord = returnRandomWord(words);  
+    splittedWord = randomWord.split('');
+
+    console.log(splittedWord);   
+    drawLines(splittedWord.length);
+
+    document.addEventListener("keyup", pressedKey);
 });
 
 var addBtn = document.querySelector("#addBtn");
@@ -29,4 +37,46 @@ function returnRandomWord(words){
     var word = words[Math.floor(Math.random()*words.length)];
 
     return word.toUpperCase();
+}
+
+function pressedKey(event){ 
+    if (!isValidKey(event.keyCode)){
+        return;
+    }
+    console.log(event.key);
+    console.log(splittedWord);
+
+    var okLetter = event.key.toUpperCase();
+
+    if (!isInSecredWord(okLetter)){
+        return;
+    }
+
+    showRightLetter(okLetter, letterIdxs);
+}
+
+function isValidKey(letterCode){
+    if(letterCode >= 65 && letterCode <= 90 || letterCode == 186){
+        return true;
+    }
+}
+
+function isInArray(letter, array){
+    if(array.indexOf(letter) == -1){
+        return false;
+    }
+        return true;
+}
+
+function isInSecredWord(letter){
+    var isInWord = false;
+    letterIdxs = [];
+
+    splittedWord.forEach((iLetter, index) => {
+        if (iLetter == letter){
+            isInWord = true;
+            letterIdxs.push(index);
+        }
+    });
+    return isInWord;
 }
